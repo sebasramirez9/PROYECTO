e3008a73ed42af6b7de7ffa5857e600145401161
sebas <sebastianramirezdiaz06@gmai.com>
2019-07-17 12:35:33 -0500

    Iniciamos

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sebas</title>
+	<link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+</head>
+<body>
+	<div class="row main-container middle-xs center-xs">
+		<div class="col-md-8 col-sm-10 col-xs-11 col-lg-7">
+			<div class="box">
+				<div class="card animated fadeInUp">
+					<header class="main-header">
+		
+		<nav class="main-nav">
+			<a href="index.php" class ="nav-link">INICIO</a>
+			<a href="menu.html"  class ="nav-link" >MENÚ</a>
+			<a href="contacto.html"  class ="nav-link" >CONTACTO</a>
+		</nav>
+	</header>
+	<header class="body">
+			
+				<header class="text-center animated flipInX delay-3">
+					<img src="./imagenes/3.png" height="100" alt="Logo de sebas">
+					<h1 class="red-text pacifico title threeD"> INICIO </h1>
+				</header>
+	</header>
+				
+	
+	</div>
+	</div>
+	</div>	
+</body>
+</html>
